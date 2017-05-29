e2cffdcc56295acbf150c3c03237afb74b7bfb55
prakashdanish <grafitykoncept@gmail.com>
2017-05-29 15:28:16 +0530

    added read loops, and updated .gitignore

--- dash.c
+++ dash.c
@@ -1,17 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define RL_BUFF_SIZE 1024
+
+char *read_line()
+{
+	int buffsize = RL_BUFF_SIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * buffsize);
+	int c;
+
+	if(!buffer)
+	{
+		fprintf(stderr, "allocation error");
+		exit(EXIT_FAILURE);
+	}
+
+	while(1)
+	{
+		c  = getchar();
+		if (c == EOF || c == '\n')
+		{
+			buffer[position] = '\0';
+			return buffer;
+		}
+		else
+		{
+			buffer[position] = c;
+		}
+		position++;
+
+		if (position >= buffsize)
+		{
+			buffsize += RL_BUFF_SIZE;
+			buffer = realloc(buffer, buffsize);
+
+			if(!buffer)
+			{
+				fprintf(stderr, "allocation error");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+
 void loop()
 {
 	char *line;
 	char **args;
-	int status;
+	int status=1;
 
 	do{
 		printf("->");
 		line = read_line();
-		args = split_line(line);
-		status = execute();
+		//args = split_line(line);
+		//status = execute();
 
-		free(line);
-		free(args);
+		//free(line);
+		//free(args);
 	}while(status);
 }
 
